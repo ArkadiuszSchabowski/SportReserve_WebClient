a3e692682971086eafa1ce9bccdd3455b3a05a22
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-06-24 20:35:29 +0200

    feat: add get user dto model

---
+++ client/src/app/models/user/get-user-dto.ts
@@ -0,0 +1,8 @@
+export class GetUserDto {
+    id: number = 0;
+    email: string = "";
+    name: string = "";
+    surname: string = "";
+    isMale: boolean | null = null;
+    dateOfBirth: string = ""
+}
