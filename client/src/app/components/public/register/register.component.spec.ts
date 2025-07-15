d855044b825ac5455cd57f790c85dc9cd6f528f6
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-15 22:39:54 +0200

    feat: create register form

--- client/src/app/components/public/register/register.component.spec.ts
+++ client/src/app/components/public/register/register.component.spec.ts
@@ -2,9 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -20,7 +23,7 @@ describe('RegisterComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      imports: [BrowserAnimationsModule, FormsModule, MaterialModule, ToastrModule],
+      imports: [BrowserAnimationsModule, FormsModule, HttpClientModule, ReactiveFormsModule, MatDatepickerModule, MatNativeDateModule, MaterialModule, ToastrModule],
       providers: [{ provide: ToastrService, useClass: MockToastrService }],
     });
     fixture = TestBed.createComponent(RegisterComponent);
