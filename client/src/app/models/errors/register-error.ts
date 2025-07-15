f0b3047eada31109b689a6d2895fcb96f6a73e19
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-15 22:40:17 +0200

    refactor: update models

--- client/src/app/models/errors/register-error.ts
+++ client/src/app/models/errors/register-error.ts
@@ -1,5 +1,9 @@
 export class RegisterError {
-    email: string | null = null;
-    password: string | null = null;
-    repeatPassword: string | null = null;
-  }
+  email: string | null = null;
+  password: string | null = null;
+  repeatPassword: string | null = null;
+  name: string | null = null;
+  surname: string | null = null;
+  gender: string | null = null;
+  dateOfBirth: string | null = null;
+}
