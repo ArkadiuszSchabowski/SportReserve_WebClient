74f07341cfc4310b98460dc94bba6c8027fff882
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-15 22:39:32 +0200

    feat: update services

--- client/src/app/_services/validator.service.ts
+++ client/src/app/_services/validator.service.ts
@@ -1,24 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ValidatorService {
   constructor() {}
-
-  public validateEmail(email: string): string | null {
-    if (!email) {
-      return 'Email address cannot be empty.';
-    }
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    let isValidEmail = emailRegex.test(email);
-
-    if (!isValidEmail) {
-      return 'Invalid email address format.';
-    }
-    return null;
-  }
 
   public validatePassword(password: string): string | null {
     if (password.length < 5 || password.length > 25) {
@@ -27,13 +14,35 @@ export class ValidatorService {
     return null;
   }
 
-  public comparePasswords(
-    password: string,
-    repeatPassword: string
-  ): string | null {
-    if (password != repeatPassword) {
-      return 'Passwords do not match.';
-    }
-    return null;
+  public matchPasswords(
+    passwordKey: string,
+    confirmPasswordKey: string
+  ): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const passwordControl = formGroup.get(passwordKey);
+      const confirmPasswordControl = formGroup.get(confirmPasswordKey);
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors['passwordMismatch']
+      ) {
+        return null;
+      }
+
+      if (password !== confirmPassword) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+
+      return null;
+    };
   }
 }
