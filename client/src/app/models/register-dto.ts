09cdc29ef8342a94d26eaa06a961e83924c623bd
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-27 15:00:12 +0200

    feat: create register form

--- client/src/app/models/register-dto.ts
+++ client/src/app/models/register-dto.ts
@@ -1,9 +1,9 @@
 export class RegisterDto {
-  email: string = '';
-  password: string = '';
-  repeatPassword: string = '';
-  name: string = '';
-  surname: string = '';
-  gender: string = '';
-  dateOfBirth: string = '';
+  email: string | null= '';
+  password: string | null = '';
+  repeatPassword: string | null = '';
+  name: string | null = '';
+  surname: string | null= '';
+  gender: string | null= '';
+  dateOfBirth: string | null= '';
 }
