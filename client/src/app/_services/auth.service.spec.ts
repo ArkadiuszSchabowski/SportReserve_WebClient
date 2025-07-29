039d65590de0dd1b5935d12c9bae5447abfcb80b
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-29 22:25:58 +0200

    chore: update test imports

--- client/src/app/_services/auth.service.spec.ts
+++ client/src/app/_services/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let toastrService: ToastrService;
+
+  class MockToastrService {
+    success(message: string) {
+      console.log(message);
+    }
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ToastrModule],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
+    });
+    toastrService = TestBed.inject(ToastrService);
     service = TestBed.inject(AuthService);
   });
 
--- client/src/app/_services/user.service.spec.ts
+++ client/src/app/_services/user.service.spec.ts
@@ -2,14 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 describe('UserService', () => {
   let service: UserService;
+  let toastrService: ToastrService;
+
+  class MockToastrService {
+    success(message: string) {
+      console.log(message);
+    }
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
     });
+    toastrService = TestBed.inject(ToastrService);
     service = TestBed.inject(UserService);
   });
 
