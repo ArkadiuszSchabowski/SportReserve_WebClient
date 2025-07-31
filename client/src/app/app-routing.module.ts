b77bf86401e9b5d4a8eee6552b876912252fe62f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-31 19:29:11 +0200

    feat: update modules

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/public/login/login.component';
-import { RegisterComponent } from './components/public/register/register.component';
+import { authGuard } from './guards/auth.guard';
+import { unauthGuard } from './guards/unauth.guard';
+import { ContactComponent } from './components/public/contact/contact.component';
 import { HomeComponent } from './components/public/home/home.component';
-
+import { LoginComponent } from './components/public/login/login.component';
+import { ProfileInformationComponent } from './components/auth/profile-information/profile-information.component';
+import { ProfileNavigationComponent } from './components/auth/profile-navigation/profile-navigation.component';
+import { ProfileReservationsComponent } from './components/auth/profile-reservations/profile-reservations.component';
+import { RacesComponent } from './components/public/races/races.component';
+import { RegisterComponent } from './components/public/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'races', component: RacesComponent },
+  {
+    path: 'profile',
+    component: ProfileNavigationComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'information', component: ProfileInformationComponent },
+      { path: 'reservations', component: ProfileReservationsComponent },
+    ],
+  },
+  {
+    path: '',
+    canActivate: [unauthGuard],
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
   { path: '**', component: HomeComponent },
 ];
 
--- client/src/app/modules/public/public.module.ts
+++ client/src/app/modules/public/public.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from 'src/app/components/public/home/home.component';
-import { LoginComponent } from 'src/app/components/public/login/login.component';
-import { RegisterComponent } from 'src/app/components/public/register/register.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ContactComponent } from 'src/app/components/public/contact/contact.component';
+import { HomeComponent } from 'src/app/components/public/home/home.component';
+import { LoginComponent } from 'src/app/components/public/login/login.component';
+import { RacesComponent } from 'src/app/components/public/races/races.component';
+import { RegisterComponent } from 'src/app/components/public/register/register.component';
 
 @NgModule({
   declarations: [
+    ContactComponent,
     HomeComponent,
     LoginComponent,
+    RacesComponent,
     RegisterComponent,
   ],
   imports: [
     CommonModule, ReactiveFormsModule, MaterialModule, MatDatepickerModule, MatNativeDateModule
   ],
   exports:[
+    ContactComponent,
     HomeComponent,
     LoginComponent,
+    RacesComponent,
     RegisterComponent,
   ]
 })
