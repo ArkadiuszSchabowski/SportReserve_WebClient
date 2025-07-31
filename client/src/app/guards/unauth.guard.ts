7e3c8d0e1ecb63b33b975b5551cd50d4ec133215
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-31 19:23:34 +0200

    feat: create auth and unauth guards

---
+++ client/src/app/guards/unauth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../_services/auth.service';
+
+export const unauthGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const toastr = inject(ToastrService);
+  const router = inject(Router);
+
+  const token = authService.currentUserSource.getValue();
+  if (token) {
+    toastr.error('Access denied. Please log out to see this page.');
+    router.navigateByUrl('/');
+    return false;
+  } else {
+    return true;
+  }
+};
