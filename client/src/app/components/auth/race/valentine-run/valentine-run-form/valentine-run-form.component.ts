837b72723e1169ce7e82520946720e4f3f2906ff
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-02 22:37:28 +0200

    refactor: move profile components for app consistency

---
+++ client/src/app/components/auth/race/valentine-run/valentine-run-form/valentine-run-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valentine-run-form',
+  templateUrl: './valentine-run-form.component.html',
+  styleUrls: ['./valentine-run-form.component.scss']
+})
+export class ValentineRunFormComponent {
+
+}
