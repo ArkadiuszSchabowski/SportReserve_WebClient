efa2b3138904f8622584e0afe58a3108ed96a4e9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-03 18:00:08 +0200

    chore: add toastr(service mock and module) to tests

--- client/src/app/components/public/races/valentine-run/valentine-run.component.spec.ts
+++ client/src/app/components/public/races/valentine-run/valentine-run.component.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ValentineRunComponent } from './valentine-run.component';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('ValentineRunComponent', () => {
   let component: ValentineRunComponent;
   let fixture: ComponentFixture<ValentineRunComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService {}
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ValentineRunComponent]
+      declarations: [ValentineRunComponent],
+      imports: [ToastrModule],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
     });
     fixture = TestBed.createComponent(ValentineRunComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
