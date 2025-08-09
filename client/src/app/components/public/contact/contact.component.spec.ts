9dfe5dd857afd756949e9884aa63d9365e022a23
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-09 22:21:52 +0200

    chore: update test imports and providers

--- client/src/app/components/public/contact/contact.component.spec.ts
+++ client/src/app/components/public/contact/contact.component.spec.ts
@@ -4,18 +4,24 @@ import { ContactComponent } from './contact.component';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrService } from 'ngx-toastr';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let toastrService: ToastrService;
+  class MockToastrService{}
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ContactComponent],
-      imports: [BrowserAnimationsModule, MaterialModule, ReactiveFormsModule],
+      imports: [BrowserAnimationsModule, HttpClientTestingModule, MaterialModule, ReactiveFormsModule],
+      providers: [{provide: ToastrService, useClass: MockToastrService }]
     });
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
