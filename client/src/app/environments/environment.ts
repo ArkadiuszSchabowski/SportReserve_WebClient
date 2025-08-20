c19bd11d06e93b450fa0da8bac25ad6491df738d
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-20 14:38:05 +0200

    feat: add raceUrl to make image display easier

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5002/'
+    apiUrl: 'https://localhost:5000/',
+    raceUrl: 'https://localhost:5002/'
   };
-  
-  // apiUrl: 'https://localhost:5000/api/apigateway/'
