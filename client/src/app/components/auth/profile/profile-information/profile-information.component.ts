153017cb99576211fb2b685abebb244e8e175d73
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-27 16:48:36 +0200

    feat: add user profile page

--- client/src/app/components/auth/profile/profile-information/profile-information.component.ts
+++ client/src/app/components/auth/profile/profile-information/profile-information.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { TokenService } from 'src/app/_services/token.service';
+import { UserService } from 'src/app/_services/user.service';
+import { GetUserDto } from 'src/app/models/user/get-user-dto';
 
 @Component({
   selector: 'app-profile-information',
   templateUrl: './profile-information.component.html',
-  styleUrls: ['./profile-information.component.scss']
+  styleUrls: ['./profile-information.component.scss'],
 })
-export class ProfileInformationComponent {
+export class ProfileInformationComponent implements OnInit {
+  email: string = '';
+  user: GetUserDto = new GetUserDto();
+  userForm = this.fb.group({
+    name: new FormControl({ value: '', disabled: true }),
+    surname: new FormControl({ value: '', disabled: true }),
+    email: new FormControl({ value: '', disabled: true }),
+    gender: new FormControl({ value: '', disabled: true }),
+    dateOfBirth: new FormControl({ value: '', disabled: true }),
+  });
 
+  constructor(
+    private tokenService: TokenService,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.getProfileInformation();
+  }
+  getProfileInformation() {
+    this.email = this.tokenService.getEmailFromToken();
+
+    this.userService.getUserByEmail(this.email).subscribe({
+      next: (response) => {
+        this.user = response;
+        this.userForm.patchValue(this.user);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
