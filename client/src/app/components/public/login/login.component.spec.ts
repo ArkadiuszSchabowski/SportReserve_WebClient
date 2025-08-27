627f981dbe2ff43cb2b5ef6c9fbb93c3df74818d
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-27 21:43:29 +0200

    refactor: remove unused forms module

--- client/src/app/components/public/login/login.component.spec.ts
+++ client/src/app/components/public/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
@@ -19,7 +19,6 @@ describe('LoginComponent', () => {
       declarations: [LoginComponent],
       imports: [
         BrowserAnimationsModule,
-        FormsModule,
         HttpClientTestingModule,
         MaterialModule,
         ReactiveFormsModule,
