1ad88af48cb4b324611b203d1c376497970a2628
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-27 16:57:39 +0200

    chore: add imports to tests

--- client/src/app/components/auth/profile/profile-information/profile-information.component.spec.ts
+++ client/src/app/components/auth/profile/profile-information/profile-information.component.spec.ts
@@ -3,18 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileInformationComponent } from './profile-information.component';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrService } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ProfileInformationComponent', () => {
   let component: ProfileInformationComponent;
   let fixture: ComponentFixture<ProfileInformationComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService{};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ProfileInformationComponent],
-      imports: [BrowserAnimationsModule, MaterialModule],
+      imports: [BrowserAnimationsModule, HttpClientTestingModule, MaterialModule, ReactiveFormsModule],
+      providers: [{provide: ToastrService, useClass: MockToastrService}]
     });
     fixture = TestBed.createComponent(ProfileInformationComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
