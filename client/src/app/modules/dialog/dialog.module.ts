a42347d63347d26d8c64c79cf46cc3616aa7a440
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-30 12:33:58 +0200

    feat: update dialog module

--- client/src/app/modules/dialog/dialog.module.ts
+++ client/src/app/modules/dialog/dialog.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DialogRemoveUserComponent } from 'src/app/components/dialog/dialog-remove-user/dialog-remove-user.component';
 import { MaterialModule } from '../material/material.module';
 import { DialogRemoveRaceComponent } from 'src/app/components/dialog/dialog-remove-race/dialog-remove-race.component';
+import { DialogUpdateRaceComponent } from 'src/app/components/dialog/dialog-update-race/dialog-update-race.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DialogRemoveUserComponent, DialogRemoveRaceComponent],
-  imports: [CommonModule, MaterialModule],
-  exports: [DialogRemoveUserComponent, DialogRemoveRaceComponent],
+  declarations: [DialogRemoveUserComponent, DialogRemoveRaceComponent,  DialogUpdateRaceComponent],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
+  exports: [DialogRemoveUserComponent, DialogRemoveRaceComponent, DialogUpdateRaceComponent],
 })
 export class DialogModule {}
