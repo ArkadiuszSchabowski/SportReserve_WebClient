fc0f6197c36bdd2f6dfc6dae3f9a056a76e4116e
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-30 21:59:57 +0200

    chore: update test imports and providers

--- client/src/app/components/dialog/dialog-add-race/dialog-add-race.component.spec.ts
+++ client/src/app/components/dialog/dialog-add-race/dialog-add-race.component.spec.ts
@@ -1,17 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DialogAddRaceComponent } from './dialog-add-race.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { MaterialModule } from 'src/app/modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DialogAddRaceComponent', () => {
   let component: DialogAddRaceComponent;
   let fixture: ComponentFixture<DialogAddRaceComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService {}
+
+  const mockDialogRef = {
+    close: jasmine.createSpy('close'),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DialogAddRaceComponent]
+      declarations: [DialogAddRaceComponent],
+      imports: [BrowserAnimationsModule, HttpClientTestingModule, MaterialModule, ReactiveFormsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: ToastrService, useClass: MockToastrService },
+      ],
     });
     fixture = TestBed.createComponent(DialogAddRaceComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
--- client/src/app/components/dialog/dialog-update-race/dialog-update-race.component.spec.ts
+++ client/src/app/components/dialog/dialog-update-race/dialog-update-race.component.spec.ts
@@ -6,11 +6,15 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 describe('DialogUpdateRaceComponent', () => {
   let component: DialogUpdateRaceComponent;
   let fixture: ComponentFixture<DialogUpdateRaceComponent>;
   const data: GetRaceViewDto = new GetRaceViewDto();
+  let toastrService: ToastrService;
+
+  class MockToastrService{}
 
   const mockDialogRef = {
     close: jasmine.createSpy('close'),
@@ -23,10 +27,12 @@ describe('DialogUpdateRaceComponent', () => {
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: data },
         { provide: MatDialogRef, useValue: mockDialogRef },
+        {provide: ToastrService, useClass: MockToastrService}
       ],
     });
     fixture = TestBed.createComponent(DialogUpdateRaceComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
