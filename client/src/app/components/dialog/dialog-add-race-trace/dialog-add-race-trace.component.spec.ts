7849223bcd42941bdcd32db352d544e3ae9b4263
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-31 21:16:24 +0200

    feat: create dialog add race trace component with logic

---
+++ client/src/app/components/dialog/dialog-add-race-trace/dialog-add-race-trace.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogAddRaceTraceComponent } from './dialog-add-race-trace.component';
+import { GetRaceDto } from 'src/app/models/race/get-race-dto';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrService } from 'ngx-toastr';
+import { MaterialModule } from 'src/app/modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('DialogAddRaceTraceComponent', () => {
+  let component: DialogAddRaceTraceComponent;
+  let fixture: ComponentFixture<DialogAddRaceTraceComponent>;
+  let toastrService: ToastrService;
+  const data: GetRaceDto = new GetRaceDto();
+  const mockDialogRef = {};
+  class MockToastrService{}
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DialogAddRaceTraceComponent],
+      imports: [BrowserAnimationsModule, HttpClientTestingModule, MaterialModule, ReactiveFormsModule],
+      providers: [
+        {provide: ToastrService, useClass: MockToastrService},
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: data }
+      ],
+    });
+    fixture = TestBed.createComponent(DialogAddRaceTraceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/dialog/dialog-add-race-trace/dialog-add-race-trace.component.ts
@@ -0,0 +1,121 @@
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { RaceTraceService } from 'src/app/_services/race-trace.service';
+import { ValidatorService } from 'src/app/_services/validator.service';
+import { AddRaceTraceDto } from 'src/app/models/race/add-race-trace-dto';
+import { GetRaceDto } from 'src/app/models/race/get-race-dto';
+import { RegistrationOption } from 'src/app/models/registration-option';
+
+@Component({
+  selector: 'app-dialog-add-race-trace',
+  templateUrl: './dialog-add-race-trace.component.html',
+  styleUrls: ['./dialog-add-race-trace.component.scss'],
+})
+export class DialogAddRaceTraceComponent {
+  form: any;
+    registrationOption: RegistrationOption[] = [
+    {value: false, viewValue: 'Close'},
+    {value: true, viewValue: 'Open'},
+  ];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: GetRaceDto,
+    private fb: FormBuilder,
+    private raceTraceService: RaceTraceService,
+    private dialogRef: MatDialogRef<DialogAddRaceTraceComponent>,
+    private validatorService: ValidatorService,
+    private toastr: ToastrService
+  ) {
+    this.form = this.fb.group({
+      location: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(100),
+        ],
+      ],
+      hourOfStart: ['', [Validators.required, validatorService.isTime]],
+      distanceKm: [
+        0,
+        [Validators.required, Validators.min(0.1), Validators.max(200)],
+      ],
+      slots: null,
+      isRegistrationOpen: [true],
+    });
+  }
+
+  addRaceTrace() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const dto: AddRaceTraceDto = {
+      location: this.form.value.location,
+      hourOfStart: this.form.value.hourOfStart,
+      distanceKm: this.form.value.distanceKm,
+      slots: this.form.value.slots,
+      isRegistrationOpen: this.form.value.isRegistrationOpen,
+      parentRaceId: this.data.id,
+    };
+
+    console.log("value:")
+    console.log(dto.isRegistrationOpen);
+
+    this.raceTraceService.add(dto).subscribe({
+      next: () => {
+        this.toastr.success(`Race trace successfully added.`);
+        this.dialogRef.close(false);
+      },
+      error: (error) => this.toastr.error(error),
+    });
+  }
+  get locationError(): string | null {
+    const control = this.form.get('location');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Location is required.';
+      }
+      if (control.errors['minlength'] || control.errors['maxlength']) {
+        return 'Location must be between 10 and 100 characters.';
+      }
+    }
+    return null;
+  }
+
+  get hourOfStartError(): string | null {
+    const control = this.form.get('hourOfStart');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Hour of start is required.';
+      }
+      if (control.errors['invalidTime']) {
+        return 'Invalid time format. Expected format is HH:mm:ss.';
+      }
+    }
+    return null;
+  }
+  get distanceKmError(): string | null {
+    const control = this.form.get('distanceKm');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Distance is required.';
+      }
+      if (control.errors['min'] || control.errors['max']) {
+        return 'Race distance must be between 0.1km and 200km.';
+      }
+    }
+    return null;
+  }
+
+  get slotsError(): string | null {
+    return null;
+  }
+
+  get registrationError(): string | null {
+    return null;
+  }
+}
