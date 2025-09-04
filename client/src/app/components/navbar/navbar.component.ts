6325ee5eb0484f1d7eceab70e97cd514fc0e4e5f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-04 17:31:09 +0200

    feat: set moderator panel function only for moderators and admins

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
 import { AuthService } from 'src/app/_services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/_services/token.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+export class NavbarComponent implements OnInit {
+  role: string | null = null;
+
+  constructor(
+    public authService: AuthService,
+    public tokenService: TokenService
+  ) {}
+  ngOnInit(): void {
+    this.setRole();
+  }
+  setRole() {
+    this.authService.setRole();
+  }
 }
