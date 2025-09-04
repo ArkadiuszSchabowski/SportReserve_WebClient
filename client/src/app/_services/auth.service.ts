fab79db9b31933eb26baeb552dbebd2ec44b5c4a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-04 17:27:37 +0200

    feat: create setRole and getRoleFromToken methods

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,10 @@ export class AuthService {
   currentUserSource = new BehaviorSubject<string | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private toastr: ToastrService) {
+  currentRoleSource = new BehaviorSubject<string | null>(null);
+  currentRole$ = this.currentRoleSource.asObservable();
+
+  constructor(private toastr: ToastrService, private tokenService: TokenService) {
     this.setUser();
   }
   logout() {
@@ -23,5 +27,10 @@ export class AuthService {
     if (token) {
       this.currentUserSource.next(token);
     }
+  }
+  setRole(){
+    const role = this.tokenService.getRoleFromToken();
+    console.log(role);
+    this.currentRoleSource.next(role);
   }
 }
--- client/src/app/_services/token.service.ts
+++ client/src/app/_services/token.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TokenService {
+export class TokenService implements OnInit {
   email = '';
-  token: string | null = '';
+  role = '';
+  token: string | null = null;
   readonly emailClaim: string =
     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress';
+  readonly roleClaim: string =
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
 
   constructor() {}
+  ngOnInit(): void {}
 
   getEmailFromToken(): string {
     this.token = localStorage.getItem('token');
@@ -20,6 +25,19 @@ export class TokenService {
 
       this.email = decodedToken[this.emailClaim];
       return this.email;
+    }
+    return '';
+  }
+
+  getRoleFromToken(): string {
+    this.token = localStorage.getItem('token');
+
+    if (this.token) {
+      let decodedToken: any = jwtDecode(this.token);
+
+      this.role = decodedToken[this.roleClaim];
+
+      return this.role;
     }
     return '';
   }
