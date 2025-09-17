05e90f90f8b1f9f427fa84535959671a67831f9c
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-17 23:11:43 +0200

    fix: add reservation url for production environment

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://localhost:5000/',
-  raceUrl: 'https://localhost:5002/'
+  raceUrl: 'https://localhost:5002/',
+  reservationUrl: 'https://localhost:5004/',
 };
