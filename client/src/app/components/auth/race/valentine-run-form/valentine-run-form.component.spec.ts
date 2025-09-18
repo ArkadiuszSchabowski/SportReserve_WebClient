f9a2a3e3e3a7cfc5bf00401cb29a86d25082b580
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-19 00:51:26 +0200

    feat: create valentine form template (without runType, valentineGadget and wantsFinisherPhoto)

--- client/src/app/components/auth/race/valentine-run-form/valentine-run-form.component.spec.ts
+++ client/src/app/components/auth/race/valentine-run-form/valentine-run-form.component.spec.ts
@@ -1,16 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ValentineRunFormComponent } from './valentine-run-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
+import { MaterialModule } from 'src/app/modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ValentineRunFormComponent', () => {
   let component: ValentineRunFormComponent;
   let fixture: ComponentFixture<ValentineRunFormComponent>;
+  let toastrService: ToastrService;
+  let activatedRoute: ActivatedRoute;
+
+  class MockToastrService {}
+  class MockActivatedRoute {
+    snapshot = {
+      paramMap: {
+        get: (key: string) => '1',
+      },
+    };
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ValentineRunFormComponent],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        MatDatepickerModule,
+        MaterialModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        { provide: ToastrService, useClass: MockToastrService },
+        { provide: ActivatedRoute, useClass: MockActivatedRoute },
+      ],
     });
     fixture = TestBed.createComponent(ValentineRunFormComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
--- client/src/app/components/auth/race/valentine-run-form/valentine-run-form.component.ts
+++ client/src/app/components/auth/race/valentine-run-form/valentine-run-form.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { RaceService } from 'src/app/_services/race.service';
+import { ReservationService } from 'src/app/_services/reservation.service';
+import { TokenService } from 'src/app/_services/token.service';
+import { UserService } from 'src/app/_services/user.service';
+import { environment } from 'src/app/environments/environment';
 import { UserInformationForm } from 'src/app/models/forms/user-information-form';
 import { ValentineRaceForm } from 'src/app/models/forms/valentine-race-form';
+import { GetRaceTraceViewDto } from 'src/app/models/race/get-race-trace-view-dto';
+import { GetRaceViewDto } from 'src/app/models/race/get-race-view-dto';
+import { ValentineRaceReservationDto } from 'src/app/models/reservation/valentine-race-reservation-dto';
+import { GetUserDto } from 'src/app/models/user/get-user-dto';
 
 @Component({
   selector: 'app-valentine-run-form',
@@ -9,7 +21,26 @@ import { ValentineRaceForm } from 'src/app/models/forms/valentine-race-form';
   styleUrls: ['./valentine-run-form.component.scss'],
 })
 export class ValentineRunFormComponent {
-  constructor(private fb: FormBuilder) {}
+  @ViewChild('stepper') stepper!: MatStepper;
+  constructor(
+    private fb: FormBuilder,
+    private tokenService: TokenService,
+    private userService: UserService,
+    private raceService: RaceService,
+    private activatedRoute: ActivatedRoute,
+    private reservationService: ReservationService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  id: number | null = null;
+  email: string = '';
+  user: GetUserDto = new GetUserDto();
+  isLinear: boolean = true;
+  race: GetRaceViewDto = {} as GetRaceViewDto;
+  raceIdString: string | null = '';
+  raceUrl: string = environment.raceUrl;
+  raceTraces: GetRaceTraceViewDto[] = [];
 
   personalInfoForm: FormGroup<UserInformationForm> = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -26,8 +57,165 @@ export class ValentineRunFormComponent {
   });
 
   raceInfoForm: FormGroup<ValentineRaceForm> = this.fb.group({
+    race: [
+      { value: '', viewValue: this.race.name, disabled: true },
+      [Validators.required],
+    ],
+    raceTrace: ['', [Validators.required]],
     runType: [''],
     valentineGadget: [''],
     wantsFinisherPhoto: [false],
   });
+
+  ngOnInit(): void {
+    this.getRace();
+    this.getProfileInformation();
+  }
+
+  getRace() {
+    this.getRaceIdFromUrl();
+    this.id = this.convertIdToNumber();
+    this.raceService.getRaceWithId(this.id).subscribe({
+      next: (response) => {
+        this.race = response;
+        this.raceTraces = response.raceTraces;
+        this.raceInfoForm.controls.race.patchValue(this.race.name);
+        this.raceInfoForm.controls.race.disable();
+      },
+    });
+  }
+  convertIdToNumber(): number {
+    return parseInt(this.raceIdString!.toString());
+  }
+
+  getRaceIdFromUrl() {
+    this.raceIdString = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  getProfileInformation() {
+    this.email = this.tokenService.getEmailFromToken();
+
+    this.userService.getUserByEmail(this.email).subscribe({
+      next: (response) => {
+        this.user = response;
+        this.personalInfoForm.patchValue(this.user);
+      },
+    });
+  }
+
+  validateRaceInformation() {
+    console.log(this.raceInfoForm.controls);
+    if (this.raceInfoForm.invalid) {
+      this.raceInfoForm.markAllAsTouched();
+      return;
+    }
+
+    this.stepper.next();
+  }
+
+  validatePersonalInformation() {
+    if (this.personalInfoForm.invalid) {
+      this.personalInfoForm.markAllAsTouched();
+      return;
+    }
+  }
+
+  sendReservation() {
+    this.validatePersonalInformation();
+
+    let dto: ValentineRaceReservationDto = {
+      userId: this.user.id,
+      raceId: this.race.id,
+      raceTraceId: parseFloat(this.raceInfoForm.value.raceTrace!),
+      valentineGadget: '',
+      runType: '',
+      wantsFinisherPhoto: false
+    };
+
+    this.reservationService.sendValentineRaceReservation(dto).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/profile/reservations');
+        this.toastr.success('Reserved successfully.')
+      },
+    });
+  }
+  get dateOfBirthError(): string | null {
+    const control = this.personalInfoForm.get('dateOfBirth');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Date of birth is required.';
+      }
+    }
+    return null;
+  }
+
+  get emailError(): string | null {
+    const control = this.personalInfoForm.get('email');
+
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Email address is required.';
+      }
+      if (control.errors['email']) {
+        return 'Invalid email address format.';
+      }
+    }
+    return null;
+  }
+
+  get genderError(): string | null {
+    const control = this.personalInfoForm.get('gender');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'You must choose a gender.';
+      }
+    }
+    return null;
+  }
+
+  get nameError(): string | null {
+    const control = this.personalInfoForm.get('name');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'User name is required.';
+      }
+      if (control.errors['minlength'] || control.errors['maxlength']) {
+        return 'User name must be between 3 and 25 characters.';
+      }
+    }
+    return null;
+  }
+
+  get raceError(): string | null {
+    const control = this.raceInfoForm.get('race');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Race is required.';
+      }
+    }
+    return null;
+  }
+
+  get raceTraceError(): string | null {
+    const control = this.raceInfoForm.get('raceTrace');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'Race trace is required.';
+      }
+    }
+    return null;
+  }
+
+  get surnameError(): string | null {
+    const control = this.personalInfoForm.get('surname');
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return 'User surname is required.';
+      }
+      if (control.errors['minlength'] || control.errors['maxlength']) {
+        return 'User surname must be between 3 and 25 characters.';
+      }
+    }
+    return null;
+  }
 }
