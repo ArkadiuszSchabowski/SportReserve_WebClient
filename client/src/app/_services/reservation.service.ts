69f7ca144f0b2744834e003ffef9f35e6e215db2
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-19 00:44:20 +0200

    feat: update http post methods

--- client/src/app/_services/reservation.service.ts
+++ client/src/app/_services/reservation.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AnimalShelterRaceReservationDto } from '../models/reservation/animal-shelter-race-reservation-dto';
+import { ValentineRaceReservationDto } from '../models/reservation/valentine-race-reservation-dto';
+import { LondonHalfMarathonReservationDto } from '../models/reservation/london-half-marathon-reservation-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +13,34 @@ export class ReservationService {
 
   constructor(private http: HttpClient) {}
 
-  SendAnimalShelterRaceReservation(dto: AnimalShelterRaceReservationDto) {
+  sendAnimalShelterRaceReservation(dto: AnimalShelterRaceReservationDto) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.post(
-      this.reservationUrl + 'api/reservation/animal-shelter-race',dto, {headers}
+      this.reservationUrl + 'api/reservation/animal-shelter-race',
+      dto,
+      { headers }
+    );
+  }
+  sendValentineRaceReservation(dto: ValentineRaceReservationDto) {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.post(
+      this.reservationUrl + 'api/reservation/valentine-race',
+      dto,
+      { headers }
+    );
+  }
+  sendLondonHalfMarathonRaceReservation(dto: LondonHalfMarathonReservationDto) {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    return this.http.post(
+      this.reservationUrl + 'api/reservation/london-half-marathon-race',
+      dto,
+      { headers }
     );
   }
 }
