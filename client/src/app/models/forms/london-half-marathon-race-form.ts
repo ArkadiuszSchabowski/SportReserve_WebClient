ac571f19733d904624486b08ae381e97d9b203e0
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-19 00:42:19 +0200

    feat: update race forms

--- client/src/app/models/forms/london-half-marathon-race-form.ts
+++ client/src/app/models/forms/london-half-marathon-race-form.ts
@@ -1,6 +1,8 @@
 import { FormControl } from '@angular/forms';
 
 export interface LondonHalfMarathonRaceForm {
+  race: FormControl<string | null>;
+  raceTrace: FormControl<string | null>;
   tShirtSize: FormControl<string | null>;
   teamName: FormControl<string | null>;
   medalGratification: FormControl<boolean | null>;
