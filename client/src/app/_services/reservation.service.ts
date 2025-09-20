85d8454c2d67056a8ef5a659c1cbc37f3fac2199
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-20 19:22:59 +0200

    feat: update fetch reservations

--- client/src/app/_services/reservation.service.ts
+++ client/src/app/_services/reservation.service.ts
@@ -4,14 +4,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AnimalShelterRaceReservationDto } from '../models/reservation/animal-shelter-race-reservation-dto';
 import { ValentineRaceReservationDto } from '../models/reservation/valentine-race-reservation-dto';
 import { LondonHalfMarathonReservationDto } from '../models/reservation/london-half-marathon-reservation-dto';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationService {
   reservationUrl = environment.reservationUrl;
+  userId: number | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  get(){
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(this.reservationUrl + `api/reservation`, {headers})
+  }
 
   sendAnimalShelterRaceReservation(dto: AnimalShelterRaceReservationDto) {
     const token = localStorage.getItem('token');
--- client/src/app/components/auth/profile/profile-reservations/profile-reservations.component.ts
+++ client/src/app/components/auth/profile/profile-reservations/profile-reservations.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from 'src/app/_services/reservation.service';
 
 @Component({
   selector: 'app-profile-reservations',
   templateUrl: './profile-reservations.component.html',
   styleUrls: ['./profile-reservations.component.scss'],
 })
-export class ProfileReservationsComponent {}
+export class ProfileReservationsComponent implements OnInit{
+  reservations: any;
+
+  constructor(private reservationService: ReservationService){
+
+  }
+
+  ngOnInit(): void {
+    this.get();
+  }
+  
+  get() {
+    this.reservationService.get().subscribe({
+      next: response => {
+        console.log(response)
+        this.reservations = response
+      },
+    })
+  }
+}
