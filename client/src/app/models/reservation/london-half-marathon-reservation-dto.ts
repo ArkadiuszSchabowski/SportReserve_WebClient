740b79cd6d9a58b0d010528e4ab17d5c870ab624
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-20 12:55:19 +0200

    fix: set tShirtSize as nullable

--- client/src/app/models/reservation/london-half-marathon-reservation-dto.ts
+++ client/src/app/models/reservation/london-half-marathon-reservation-dto.ts
@@ -1,7 +1,7 @@
 import { ReservationBase } from "./base/reservation-base";
 
 export interface LondonHalfMarathonReservationDto extends ReservationBase {
-    tShirtSize: string;
+    tShirtSize: string | null;
     teamName: string | null;
     medalGratification: boolean;
 }
